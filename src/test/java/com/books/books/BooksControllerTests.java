2e3b5ee2689978d4513e185202308e7d56e363e8
wojtekbakun <wojciechzsee@gmail.com>
2024-09-20 14:29:16 +0200

    added comments to unit tests

--- src/test/java/com/books/books/BooksControllerTests.java
+++ src/test/java/com/books/books/BooksControllerTests.java
@@ -19,30 +19,46 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.springframework.http.MediaType;
 
+// Unit tests for the BooksController class
+
+// Load only the BooksController class 
 @WebMvcTest(BooksController.class)
+
+// Automatically configure the MockMvc to test the BooksController class
 @AutoConfigureMockMvc
 class BooksControllerTests {
 
+    // Inject the MockMvc instance
     @Autowired
     private MockMvc mockMvc;
 
+    // Inject the ObjectMapper instance
     @Autowired
     private ObjectMapper objectMapper;
+
+    // Mock the BooksRepository class
     @MockBean
     private BooksRepository booksRepository;
 
+    // Test the home method
     @Test
     void testHome() {
+        // Create a new BooksController instance
         BooksController booksController = new BooksController(null); // Arrange
+        // Call the home method
         String response = booksController.home(); // Act
+        // Check if the response is correct
         assertEquals("Welcome to the Books API!", response); // Assert
     }
 
     @Test
     void testGetBooks() throws Exception {
+        // Create a new Books instance
         Books bookMock = new Books("Młody człowiek i jezioro", "Jerzy Jurek", 1410, null, false);
+        // Find title of the book in mocked repository and compare it with the bookMock when the findByTitle method is called
         when(booksRepository.findByTitle("Młody człowiek i jezioro")).thenReturn(bookMock);
 
+        // Perform a GET request to test findByTitle method
         mockMvc.perform(get("/books/title/Młody człowiek i jezioro"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(
@@ -51,17 +67,22 @@ class BooksControllerTests {
 
     @Test
     void testGetBooksByAuthor() throws Exception {
+        // Create a new Books instance
         Books bookMock = new Books("Młody człowiek i jezioro", "Jerzy Jurek", 1410, null, false);
-        when(booksRepository.findByAuthor("Jerzy Jurek")).thenReturn(java.util.List.of(bookMock));
+        Books bookMock2 = new Books("Młody człowiek i morze", "Jerzy Jurek", 1411, null, false);
+        // Find all books of the author and compare them with mocks
+        when(booksRepository.findByAuthor("Jerzy Jurek")).thenReturn(java.util.List.of(bookMock, bookMock2));
 
+        // Perform a GET request to test findByAuthor method
         mockMvc.perform(get("/books/author/Jerzy Jurek"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(
-                        "[{'title': 'Młody człowiek i jezioro', 'author': 'Jerzy Jurek', 'year': 1410, 'rating': null, 'ispdf': false}]"));
+                        "[{'title': 'Młody człowiek i jezioro', 'author': 'Jerzy Jurek', 'year': 1410, 'rating': null, 'ispdf': false}, {'title': 'Młody człowiek i morze', 'author': 'Jerzy Jurek', 'year': 1411, 'rating': null, 'ispdf': false}]"));
     }
 
     @Test
     void testGetBooksByYear() throws Exception {
+        // Create a new Books instance
         Books bookMock = new Books("Młody człowiek i jezioro", "Jerzy Jurek", 1410, null, false);
         when(booksRepository.findByYear(1410)).thenReturn(java.util.List.of(bookMock));
 
@@ -73,6 +94,7 @@ class BooksControllerTests {
 
     @Test
     void testGetBooksByRating() throws Exception {
+        // Create a new Books instance
         Books bookMock = new Books("Młody człowiek i jezioro", "Jerzy Jurek", 1410, null, false);
         when(booksRepository.findByRating(5)).thenReturn(java.util.List.of(bookMock));
 
@@ -84,6 +106,7 @@ class BooksControllerTests {
 
     @Test
     void testGetBooksByIspdf() throws Exception {
+        // Create a new Books instance
         Books bookMock = new Books("Młody człowiek i jezioro", "Jerzy Jurek", 1410, null, false);
         when(booksRepository.findByIspdf(false)).thenReturn(java.util.List.of(bookMock));
 
@@ -95,6 +118,7 @@ class BooksControllerTests {
 
     @Test
     void testInsertNewRating() throws Exception {
+        // Create a new Books instance
         Books bookMock = new Books("Młody człowiek i jezioro", "Jerzy Jurek", 1410, 5.0f, false);
         when(booksRepository.findByTitle("Młody człowiek i jezioro")).thenReturn(bookMock);
 
@@ -104,9 +128,13 @@ class BooksControllerTests {
                         "{'title': 'Młody człowiek i jezioro', 'author': 'Jerzy Jurek', 'year': 1410, 'rating': 5.0, 'ispdf': false}"));
     }
 
-  @Test
+    @Test
     void testAddBook() throws Exception {
+        // Create a new Books instance
         Books bookMock = new Books("Kapsztad", "Zdroje", 2011, null, true);
+        
+        // Compare book returned in response with the new instance
+        // Method save should return bookMock regardless of book class
         when(booksRepository.save(any(Books.class))).thenReturn(bookMock);
 
         mockMvc.perform(post("/books")
